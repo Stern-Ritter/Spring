79b4ce1d609b4c24dd8376f555b509cc1b758496
Kayen <fadeew.k@mail.ru>
2020-12-31 21:06:19 +0300

    Homework 7

--- lesson-6-spring-boot-app/src/main/java/ru/geekbrains/persist/entity/Product.java
+++ lesson-6-spring-boot-app/src/main/java/ru/geekbrains/persist/entity/Product.java
@@ -1,6 +1,11 @@
 package ru.geekbrains.persist.entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
